b249771224d6162b9a712793bbcc2a99824a73e1
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2025-08-17 15:18:30 +0600

    feat(tenant): add fastapi-tenant package
    - Added core package files
    - Implemented CLI
    - Defined middleware, resolvers, and settings
    - Created tenant context and dependency injection
    - Included base classes and example resolvers

---
+++ src/fastapi_tenant/__init__.py
@@ -0,0 +1,17 @@
+from .cli import app as cli
+from .db import tenant_session_dependency as tenant_dep
+from .middleware import TenantMiddlewareChain
+from .resolvers import BaseResolver, PathResolver, SubdomainResolver
+from .settings import settings
+from .tenant_context import Tenants
+
+__all__ = [
+    "settings",
+    "Tenants",
+    "TenantMiddlewareChain",
+    "BaseResolver",
+    "SubdomainResolver",
+    "PathResolver",
+    "tenant_dep",
+    "cli",
+]
